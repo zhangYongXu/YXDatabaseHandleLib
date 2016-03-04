d7ebc650c3a6c04f5fe6012c4ca83adb26ab4271
zyx612423 <790372708@qq.com>
2016-03-04 15:43:57 +0800

    YXDatabaseHandleLib打包framework

---
+++ YXDatabaseHandleLib/YXDatabaseHandleLib.h
@@ -0,0 +1,17 @@
+//
+//  YXDatabaseHandleLib.h
+//  YXDatabaseHandleLib
+//
+//  Created by 拓之林 on 16/3/1.
+//  Copyright © 2016年 拓之林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YXDatabaseHandleLib.
+FOUNDATION_EXPORT double YXDatabaseHandleLibVersionNumber;
+
+//! Project version string for YXDatabaseHandleLib.
+FOUNDATION_EXPORT const unsigned char YXDatabaseHandleLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YXDatabaseHandleLib/PublicHeader.h>
